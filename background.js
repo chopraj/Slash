fb8782be6672ad0920b0732aa93c4b6f6eabd0be
Raj Chopra <raj.chopra@vanderbilt.edu>
2022-06-13 00:00:41 -0700

    background.js messaging updated

--- background.js
+++ background.js
@@ -1,1 +1,7 @@
 // Runs as soon as the extension is installed or refreshed
+
+chrome.runtime.onMessage.addListener(recieveMessage)
+
+function recieveMessage(request, sender, response) {
+    // code here to summarize text
+}
--- content.js
+++ content.js
@@ -3,5 +3,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command == 'startSummary') {
         console.log('from content script', request.length)
         let seletedText = window.getSelection().toString()
+        if (selectedText.length > 0) {
+            message = {
+                text: selectedText,
+                length: request.length
+            }
+            chrome.runtime.sendMessage(message)
+        }
     }
 })
