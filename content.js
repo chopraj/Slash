582aff6130d63c0bb4e7ba3fa19148e61316e746
Raj Chopra <raj.chopra@vanderbilt.edu>
2022-06-12 23:53:36 -0700

    updated content script

--- content.js
+++ content.js
@@ -2,5 +2,6 @@ console.log("content script")
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command == 'startSummary') {
         console.log('from content script', request.length)
+        let seletedText = window.getSelection().toString()
     }
 })
