8d0a6be60f3062c92f4977c2abf06c18c14ea5d2
Raj Chopra <raj.chopra@vanderbilt.edu>
2022-06-19 23:36:42 -0700

    fixed content.js bug

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ console.log("content script")
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command == 'startSummary') {
         console.log('from content script', request.length)
-        let seletedText = window.getSelection().toString()
+        let selectedText = window.getSelection().toString()
         if (selectedText.length > 0) {
             message = {
                 text: selectedText,
